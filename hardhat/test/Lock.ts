c722b422dfffe5e5d688902fd430117a6a371f20
tan jo <tanmayjoddar17@gmail.com>
2025-06-16 03:25:04 +0530

    hardhar fixed

--- hardhat/test/Lock.ts
+++ hardhat/test/Lock.ts
@@ -5,6 +5,7 @@ import {
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
+import { Lock } from "../typechain-types";
 
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.

64daf80ae757c9106dc3c539558b4b01218f279e
tan jo <tanmayjoddar17@gmail.com>
2025-06-19 21:33:47 +0530

    docker king

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# This is a root-level script for Render to start the Go application
-
-# Use default port 8080 if not specified by Render
+# Use default port if not provided
 export PORT=${PORT:-8080}
 
 # Run the Go binary
-exec /app/out
+exec /app/server
